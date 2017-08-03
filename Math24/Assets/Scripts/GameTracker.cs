448195b20c57be2bc6d36cb9a0e572da7c574842
Pattnatt <jittapat007@gmail.com>
2017-08-03 21:36:17 +0700

    Add random number & timer

---
+++ Math24/Assets/Scripts/GameTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTracker : MonoBehaviour {
+
+    public Text timerIntText;
+    public Text timerFloatText;
+    public Number number;
+    public float readyTime = 5;
+    public float gameTime = 60;
+    public float currentTime;
+    // Use this for initialization
+    void Start () {
+        currentTime = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;;
+        timerIntText.text = currentTime.ToString("F0");
+        timerFloatText.text = ((int)((currentTime % 1f) * 100)).ToString("00");
+    }
+}
---
+++ Math24/Assets/Scripts/Number.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Number : MonoBehaviour
+{
+    public int totalNumber = 4;
+
+    private int[] value;
+    public Text[] numberText;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        value = new int[totalNumber];
+        for (int i = 0; i < totalNumber; i++)
+            value[i] = 9;
+    }
+
+    public void NewRandom()
+    {
+        RandomValue();
+        SetText();
+    }
+
+    private void RandomValue()
+    {
+        for (int i = 0; i < totalNumber; i++)
+            value[i] = Random.Range(1, 10);
+    }
+
+    private void SetText()
+    {
+        for (int i = 0; i < totalNumber; i++)
+            numberText[i].text = value[i].ToString();
+    }
+}
